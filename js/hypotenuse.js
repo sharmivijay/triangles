c2ab57f2e48bd93040604d40ca47917aad4b91db
sharmivijay <sharmiyogu@gmail.com>
2021-09-28 16:08:57 +0530

    styles - decorate the pages

--- js/hypotenuse.js
+++ js/hypotenuse.js
@@ -1,5 +1,5 @@
-var inputSide1 = document.querySelector(".input-side1");
-var inputSide2 = document.querySelector(".input-side2");
+var inputSide1 = document.querySelector("#input-side1");
+var inputSide2 = document.querySelector("#input-side2");
 var btnCheck =  document.querySelector("#btn-check");
 var outputText = document.querySelector("#output-msg");
 
@@ -7,9 +7,12 @@ btnCheck.addEventListener("click", calculateHypotenuse);
 
 function calculateHypotenuse(){
    var hypotenuse = 0;
-   var a = Number(inputSide1.value); 
-   var b = Number(inputSide2.value);
+   var a = 0;
+   var b = 0;
+   a=Number(inputSide1.value); 
+   b = Number(inputSide2.value);
    var sumOfSides = (a*a) + (b*b);
-   outputText.innerText = "The length of the hypotenuse: "+Math.sqrt(sumOfSides);
+   var hypotenuse = Math.sqrt(sumOfSides);
+   outputText.innerText = "The length of the hypotenuse: "+ hypotenuse +"cm";
 }
 
